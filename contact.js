09e83dcb458b2fa09da028daf64dc972b63645be
Nitik-sharma <sharmanitik36@gmail.com>
2023-07-11 23:39:29 +0530

    add Phone icon

--- contact.js
+++ contact.js
@@ -18,8 +18,6 @@ function sendMail() {
       alert("Your message was successfully sent!");
     })
     .catch((err) => console.log(err));
-
-  console.log("hii");
 }
 
 const btn = document.getElementById("send");
@@ -27,3 +25,15 @@ btn.addEventListener("click", (e) => {
   e.preventDefault();
   sendMail();
 });
+var phoneNo = "8708508021";
+const phone = document.getElementById("phone");
+
+phone.addEventListener("click", (e) => {
+  e.preventDefault();
+  makeCall();
+  console.log("hii");
+});
+
+function makeCall() {
+  window.location.href = "tel:" + phoneNo;
+}
